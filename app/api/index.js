730cd1a49f4a95d48eda9516c1d74001f312dd6c
kinsteronline <todd.makinster@gmail.com>
2019-01-22 05:06:03 -0600

    Add some minimal logging to a server

--- app/api/index.js
+++ app/api/index.js
@@ -1,13 +1,26 @@
 
 import express from 'express'
-import pino from 'express-pino-logger'
+import pino from 'pino'
+import expressPino from 'express-pino-logger'
 
 import * as db from './db'
 
 import create from './create'
 import list from './list'
 
+const {
+  NODE_ENV = 'development'
+} = process.env
+
+const logger = pino({
+  enabled: NODE_ENV !== 'test',
+  prettyPrint: NODE_ENV === 'development',
+  levelFirst: NODE_ENV === 'development'
+})
+
 const api = express()
+
+api.use(expressPino({ logger }))
 
 api.use('/records', create({ db }))
 api.use('/records', list({ db }))
